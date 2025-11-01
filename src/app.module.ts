fc998bc4ee0e55a45b0350c9673753bd3f027f09
UsPortatil <jonathariflo@gmail.com>
2025-11-01 10:39:16 -0600

    integrate orm table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './app.entity';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),
+  TypeOrmModule.forFeature([Task])
+],
   controllers: [AppController],
   providers: [AppService],
 })
